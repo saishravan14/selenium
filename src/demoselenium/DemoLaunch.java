a1095424eaf14323bfe0a827974fe2d2a034febf
sai <sai@shravan>
2021-12-27 19:31:24 +0530

    commit

--- src/demoselenium/DemoLaunch.java
+++ src/demoselenium/DemoLaunch.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DemoLaunch {
 public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
-	ChromeDriver driver = new ChromeDriver(); // chrome driver
+	ChromeDriver driver = new ChromeDriver(); //  object of chrome driver
 }
 }
